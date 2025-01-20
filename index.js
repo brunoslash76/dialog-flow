246a52fd029f77918f0674ec3a9992c0de79b0e6
Bruno Russo <bruno@jumpstartmd.com>
2025-01-20 14:54:40 -0300

    feat: add dynamic attribute setting to DSF WC

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ function init() {
     get: (searchParams, prop) => searchParams.get(prop),
   });
 
-  const allParams = params.getAll()
-  console.log(allParams)
+  const agentId = params['agent-id']
+  const projectId = params['project-id']
+
+  const dfComponent = document.querySelector('#dialog-flow')
+
+  if (!agentId || !projectId) {
+    alert("Agent ID or Project ID missing, Dialog Flow won't be working properlly!")
+  }
+
+  dfComponent.setAttribute('project-id', projectId)
+  dfComponent.setAttribute('agent-id', agentId)
+
+  console.log(dfComponent)
 }
 
-init()
+document.addEventListener('DOMContentLoaded', init)
